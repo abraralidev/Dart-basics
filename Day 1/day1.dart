49d1f330cde5ae94b04deb68742b7202fbe8be6a
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-18 19:13:40 +0500

    Add files via upload

--- Day 1/day1.dart
+++ Day 1/day1.dart
@@ -1,5 +1,4 @@
 import 'dart:ffi';
-
 void main(){
 // int 
   int a = 79;
@@ -46,5 +45,49 @@ String n = "Abrar Ali";
 print("My name is $n");
 // if we find the the length of string use ${variable_name.lenght};
 print("String Size is  ${n.length}");
+//---------------------------------------------------------------------------------------------------------------
+// var keyword
+// A way to declare a variable without specifying its type.
+// For example 
+//------------------------Int-----------------
+var t = 12;
+// Here "t" is use as a int
+print(t);
+// If we want to check the type of "var"
+print(t.runtimeType);  
+
+//-------------------------Double--------------
+var r = 89.9;
+// Here "r" is use as a double
+print(r);
+// If we want to check the type of "var"
+print(r.runtimeType);
+//-------------------------boolean--------------
+var q = true;
+var w = false;
+q = false;
+print(q);
+print(w);
+print(q.runtimeType);
+print(w.runtimeType);
+//-------------------------String--------------
+var f = "Abrar";
+f = "Abrar Ali";
+print(f);
+print(f.runtimeType);
+// Now we declare a dynamic type variable
+var d ;
+d = 10;
+print(d);
+print(d.runtimeType);
+d = "Abrar";
+print(d);
+print(d.runtimeType);
+d = true;
+print(d);
+print(d.runtimeType);
+d = 54.4;
+print(d);
+print(d.runtimeType);
 
 }
