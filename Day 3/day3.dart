e65ffc77118e8f83ab43c9717763e5a72a97ba37
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-20 00:47:43 +0500

    day3

---
+++ Day 3/day3.dart
@@ -0,0 +1,21 @@
+void main(){
+  //------------------------- if else Statement----------------------------
+ /* 
+ ---syntax of if else statement 
+if(test expression){
+// statement
+}else{
+   //statement
+   }
+
+*/
+//example of if else statement
+int a = 5;
+ if (a==5) {
+   print(true);
+ } 
+ else {
+   print(false);
+ }
+ 
+}
