e97a641d34ef3352855c4aebcfca56ee298d02d3
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-19 16:42:00 +0500

    day 2

---
+++ Day 2/day2.dart
@@ -0,0 +1,21 @@
+void main() {
+  // In dynamic data type we can use one variable override for example 
+  dynamic
+  //int
+   a = 10;
+  print(a);
+  print(a.runtimeType);
+  //string
+  a= "Ali";
+  print(a);
+  print(a.runtimeType);
+  //bool
+  a=true;
+  print(a);
+  print(a.runtimeType);
+  a= 55.36;
+  print(a);
+  print(a.runtimeType);
+  
+
+}
