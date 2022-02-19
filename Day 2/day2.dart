4399436da8099ec7e568882cfb780670016c89d3
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-19 17:42:22 +0500

    Add files via upload

--- Day 2/day2.dart
+++ Day 2/day2.dart
@@ -1,5 +1,6 @@
 void main() {
-  // In dynamic data type we can use one variable override for example 
+  // The dynamic keyword can also be used as a type annotation explicitly.
+  // for example 
   dynamic
   //int
    a = 10;
@@ -16,6 +17,24 @@ void main() {
   a= 55.36;
   print(a);
   print(a.runtimeType);
+  //----------------------Difference Between Var & Dynamic--------------------------->
+  // -Var
+  // var variable once assigned, the type can not change 
+  // -Dynamic
+  // dynamic variable type can change over time
+  //------------------------Constant-------------------------------------
+  // -----------Final
+  // in final keyword if we declare one variable then we can not use same variable  
+  final c= 20;
+  print(c);
+  // n=20;    here the example of final if uncomment it show error 
+  // -----------const 
+  // stop overriding 
+  // -------------------Difference Between Const & Final------------------------------>
+  // The Const keyword in Dart behaves exactly like the final keyword. 
+  // The only difference between final and const is that the const makes the variable constant from compile-time only.
+
+
   
 
 }
