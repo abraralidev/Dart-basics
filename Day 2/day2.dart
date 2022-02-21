edde91689db7ec62d7e0d7e5444afeb5fcc9d827
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-21 16:06:49 +0500

    Add files via upload

--- Day 2/day2.dart
+++ Day 2/day2.dart
@@ -1,4 +1,5 @@
 void main() {
+
   // The dynamic keyword can also be used as a type annotation explicitly.
   // for example 
   dynamic
@@ -24,12 +25,19 @@ void main() {
   // dynamic variable type can change over time
   //------------------------Constant-------------------------------------
   // -----------Final
-  // in final keyword if we declare one variable then we can not use same variable  
-  final c= 20;
+
+  //  When we do not want to change a variable in the future then we use final and const. It can be used in place of var or in addition to a type.
+  // A final variable can be set only one time where the variable is a compile-time constant. 
+  final c= "Abrar";
+  final d= "How are you";
   print(c);
-  // n=20;    here the example of final if uncomment it show error 
+  print(d);
+
   // -----------const 
-  // stop overriding 
+  // The const is used to create compile-time constants. We can declare a value to compile-time constant such as number, string literal, a const variable, etc.
+  const v = 1000;
+  print(v);
+  // The const keyword is also used to create a constant value that cannot be changed after its creation.
   // -------------------Difference Between Const & Final------------------------------>
   // The Const keyword in Dart behaves exactly like the final keyword. 
   // The only difference between final and const is that the const makes the variable constant from compile-time only.
