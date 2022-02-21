10e1a45c7f40b7d3332041cd5fa785e67563d133
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-22 01:34:39 +0500

    operators

--- Day 3/day3.dart
+++ Day 3/day3.dart
@@ -1,47 +1,96 @@
 
 
 void main(){
+  
+// Dart Operators
+// An operator is a symbol that is used to manipulating the values or performs operations on its
+// operand. The given expression: 5+4, in this expression, 5 and 4 are operands and "+" is the operator.
+
+//  --Types of Operators
+// Dart supports the following types of operators.
+
+// 1- Arithmetic Operators
+// for example
+int x=10 , y=5 ;
+//add (+) It adds the left operand to the right operand.
+print(x+y);
+
+//subtract (-) It subtracts the right operand from the left operand.
+print(x-y);
+
+//multiply (*)	It multiplies the one operand to another operand.
+print(x*y);
+
+// divide (/) It divides the first operand by the second operand and returns quotient.
+print(x/y);
+
+// modulus (%) means It returns a reminder after dividing one operand to another.
+print(x%y);
+
+// Division (~/) 	It divides the first operand by the second operand and returns integer quotient.
+print(x~/y);
+
+// Unary ->	It is used with a single operand changes the sign of it.
+print(-x-y);
+
+// -----------------Dart Unary Operators (post and prefix)
+// In Java, there are ++ and -- operators are known as increment and decrement operators and also known as unary operators, 
+// respectively. Unary operators, operate on single operand where ++ adds 1 to operands and -- subtract 1 to operand respectively.
+// The unary operators can be used in two ways - postfix and prefix. If ++ is used as a postfix(like x++), it returns the 
+// value of operand first then increments the value of x. If -- is used as a prefix(like ++x), it increases the value of x.
+int p = 30;   
+   print(p++);                  //The postfix value  
+     
+int o = 25;  
+print(++o);                 //The prefix value,  
+         
+int z = 10;  
+print(--z);                  //The prefix value  
+  
+int u = 12;                                           
+   print(u--);    }           //The postfix value  
+
+
+// 2- Assignment Operators
+
+
+// 3- Relational Operators
+// 4- Type test Operators
+// 5- Logical Operators
+// 6- Bitwise Operator
+// 7- Conditional Operators
+// 8- Casecade notation(..) Operators
   //------------------------- Statements----------------------------
-  // if statement 
-  int a = 5;
- if (a==5) {
-   print(true);
- }
-  // if else statement
-  int b = 10;
-  if (b==10){
-    print(true);
-  }else
-  {
-    print(false);
-  }
- /* 
- ---syntax of if else statement 
-if(test expression){
-// statement
-}else{
-   //statement
-   }
 
-*/
-
- // if else if statement
- //for example
-var marks = 76;     
-if(marks > 85)  
-{  
-       print("Excellent");  
-}  
- else if(marks>75)  
-{  
-      print("Very Good");  
-}   
-else if(marks>65)  
-{  
-      print("Good");  
-}  
-else  
- {  
-      print("Average");  
-}  
-}  
+//   // if statement 
+//   int a = 5;
+//  if (a==5) {
+//    print(true);
+//  }
+//   // if else statement
+//   int b = 10;
+//   if (b==10){
+//     print(true);
+//   }else
+//   {
+//     print(false);
+//   }
+//     // if else if statement
+//  //for example
+// var marks = 74;     
+// if(marks > 85)  
+// {  
+//        print("Excellent");  
+// }  
+//  else if(marks>75)  
+// {  
+//       print("Very Good");  
+// }   
+// else if(marks>65)  
+// {  
+//       print("Good");  
+// }  
+//  else  
+//  {  
+//       print("Average");  
+// }           
