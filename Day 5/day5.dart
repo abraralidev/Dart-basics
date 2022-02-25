edf206767c1fbc45386304b37d391930ae4b20fd
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-25 23:56:02 +0500

    Add files via upload

---
+++ Day 5/day5.dart
@@ -0,0 +1,17 @@
+void main() { 
+  // do while loop
+  //Dart do while loop executes a block of the statement first and then checks the condition. If the condition returns true,
+  // then the loop continues its iteration. It is similar to Dart while loop but the only difference is,
+  // in the do-while loop a block of statements inside the body of loop will execute at least once. 
+ int i = 1;  
+print("Dart do-while loop example");  
+   
+do{  
+          
+          
+        print(i);  
+        i++;  
+  
+}while(i<=20);  
+print("The loop is terminated");  
+}  
