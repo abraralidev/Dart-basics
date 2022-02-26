14207e15e6bb13e0dcb435ac55cd26a23aeb39b2
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-26 23:35:55 +0500

    Add files via upload

--- Day 5/day5.dart
+++ Day 5/day5.dart
@@ -73,4 +73,8 @@ list.forEach((element) {
   print('${list.indexOf(element)}:$element');
 });
 
+// Main() function 
+//The main() function is the top-level function of the Dart. It is the most important and 
+//vital function of the Dart programming language. The execution of the programming starts with the main() function. 
+
 }
