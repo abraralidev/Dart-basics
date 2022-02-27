7d735906af7473c0d68c94d468cbc006e399ed59
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-27 14:50:58 +0500

    Add files via upload

--- Day 7/day7.dart
+++ Day 7/day7.dart
@@ -3,13 +3,28 @@ class cars {
   int Engine;
   int doors;
   //constructor
-  
+  //constructor is a different type of function which is created with same name as its class name. The constructor is used to initialize an object when it is created. 
+
+  // Rules of constructor
+  // 1- The constructor name should be same as the class name.
+  // 2- The constructor doesn't have explicit return type.
+
+  // Types of constructors
+// 1- Default constructor or no-arg constructor.
+// A Constructor which has no parameter is called default constructor or no-arg constructor.
+
+// 2- Parameter constructor
+// We can also pass the parameters to a constructor that type of constructor is called parameterized constructor.
+
+// 3- Named constructor
+// The named constructors are used to declare the multiple constructors in single class. 
 cars({
   required this.Carname,
   required this.Engine,
   required this.doors,
 });
-
+// this keyword
+// The this keyword is used to refer the current class object. It indicates the current instance of the class, methods, or constructor. It can be also used to call the current class methods or constructors.
 void printcar(){
 print("Car name:${this.Carname}\n Engine:${this.Engine}\n doors:${this.doors}");
 }
