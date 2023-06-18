e57fd00594e00f50fd72df80c64e3483515cf7d0
Abrar Ali <91776475+abraralidev@users.noreply.github.com>
2023-06-18 02:11:27 -0700

    Update practice.dart

--- Assignment/practice.dart
+++ Assignment/practice.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
 
   //command for run program 
@@ -10,6 +12,15 @@ void main() {
 
 // Create a program that asks the user to enter their name and their age.
 // Print out a message that tells how many years they have to be 100 years old.
+  // solution for checking
+    //   stdout.write("What's your name? ");
+    // String? name = stdin.readLineSync();
+
+    // print("Hi, $name! What is your age?");
+    // int age = int.parse(stdin.readLineSync()!);
+
+    // int yearsToHunderd = 100 - age;
+    // print("$name, You have $yearsToHunderd years to be 100");
 
 //============== Exercise 2
 
